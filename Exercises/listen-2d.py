e1f2e06c216d7a5acde64721838686ec6d0adffa
Alejandra <alejandra@.ch>
2026-09-10 19:52:59 +0200

    pretty_print version 1
    matrix in pretty_print

--- Exercises/listen-2d.py
+++ Exercises/listen-2d.py
@@ -1,5 +1,7 @@
 def pretty_print(mat2d):
     print(mat2d)
 
-pretty_print([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-pretty_print([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6]])
+pretty_print([matrix])
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+pretty_print([matrix1])
+matrix1 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6]]
