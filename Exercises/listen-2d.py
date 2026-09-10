3639e958c562efb304802596459958b57f9d8db2
Alejandra <alejandra@.ch>
2026-09-10 20:43:14 +0200

    Aufgabe 2 finished

--- Exercises/listen-2d.py
+++ Exercises/listen-2d.py
@@ -1,5 +1,5 @@
 #Aufgabe 1 Pretty_print
-
+'''
 def pretty_print(mat2d):
     for row in mat2d:
         print(row)
@@ -13,5 +13,11 @@ pretty_print(matrix1)
 #Achtung nicht `pretty_print` ([matrix]) schreiben weil sonst [matrix] als 1 Element angeschaut wird!
 
 #Aufgabe 2 
+'''
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        print(f'[i: {i}, j: {j}] =', matrix[i][j])
 
 
